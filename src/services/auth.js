cfcfdd692bb7f4d3e1c1b1b531a676c9e30e0dc7
jossdz <carloscorrea.dev@gmail.com>
2020-09-18 18:01:52 -0500

    true for wcreds

--- src/services/auth.js
+++ src/services/auth.js
@@ -8,7 +8,7 @@ const baseURL =
     : "https://vast-garden-66060.herokuapp.com/auth"
 const authService = axios.create({
   baseURL,
-  crossDomain: true
+  withCredentials: "true"
 })
 // Nosotros vamos a mandar las credenciales siempre que utilicemos al usuario en sesion o querramos utilizar la sesion
 
