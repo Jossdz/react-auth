548ec1cf79b0543d371dc523ea50974262e473b4
jossdz <carloscorrea.dev@gmail.com>
2020-09-18 17:11:08 -0500

    back url

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseURL = "http://localhost:3000/auth"
+const baseURL = "https://vast-garden-66060.herokuapp.com/auth"
 const authService = axios.create({
   baseURL,
   withCredentials: true
